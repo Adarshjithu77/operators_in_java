3c18498683cda61c7797f0560e2381b04731d2d4
Adarshjithu77 <adarshjithu77@gmail.com>
2025-07-04 12:58:58 +0530

    first commit

---
+++ qstn1.js
@@ -0,0 +1,10 @@
+
+function sum(a,b){
+    const sum=a+b
+    return sum%2==0 ? "even":"odd"
+
+    
+
+}
+console.log(sum(10,20))
+console.log(sum(10,3))
---
+++ script.js
@@ -0,0 +1,72 @@
+let number1=65;
+let number2=75;
+
+//Addition
+
+let sum=number1+number2;
+console.log(sum);
+
+//substraction
+
+let difference=number1-number2;
+console.log(difference);
+
+//multiplication
+
+let product=number1*number2;
+console.log(product);
+
+//division
+
+let qoutient=number1/number2;
+console.log(qoutient);
+
+//modulus
+
+let reminder=number1%number2;
+console.log(reminder);
+
+//increment operator
+
+let num3=10;
+num3++;
+console.log(num3,"after increment")
+
+//decrement
+
+let num4=3;
+num4--;
+console.log(num4,"after decrement");
+
+
+//logical operator
+let a = 20;
+let b = 20;
+
+// > GREATER THAN OPERATOR
+// < LESS THAN OPERATOR
+//comparison operator
+console.log(a > 0);
+console.log(a<0);
+
+// >= GREATER THAN OR EQUAL TO OPERATOR
+console.log(a >= b);
+
+// <= LESS THAN OR EQUAL TO OPERATOR
+console.log(a <= b);
+
+// LOGICAL AND OPERATOR -----&&
+
+console.log(a >= b && a == 20);
+
+// LOGICAL OR OPERATOR -----
+
+console.log(a > b || a == 20)
+
+//comparison operators
+
+let c=20;
+let d=20;
+
+console.log(a==b,"comparison" && a==20)
+
